028f6301650f4e5b62101c941a00ba2ee9cc2e84
Takafumi Arakaki <aka.tkf@gmail.com>
2022-04-26 08:07:30 -0700

    Add include_serial (#6)

--- benchmark/ParallelIDMappingsBenchmarks/src/bench_pool.jl
+++ benchmark/ParallelIDMappingsBenchmarks/src/bench_pool.jl
@@ -9,6 +9,16 @@ function setup(;
     smoke = false,
     nitems = smoke ? 10 : 2^22,
     ps = smoke ? [0.1] : [1e-4, 1e-5, 1e-6],
+    include_serial = true,
+    fs = filter!(
+        !isnothing,
+        Any[
+            include_serial ? ParallelIDMappings.pool_serial : nothing,
+            ParallelIDMappings.pool_localcopies,
+            ParallelIDMappings.pool_concurrentdict,
+            ParallelIDMappings.pool_leftright,
+        ],
+    ),
     options...,
 )
     CACHE[] = Dict(
@@ -20,12 +30,7 @@ function setup(;
     suite = BenchmarkGroup()
     for p in ps
         s1 = suite["p=$p"] = BenchmarkGroup()
-        for f in [
-            ParallelIDMappings.pool_serial,
-            ParallelIDMappings.pool_localcopies,
-            ParallelIDMappings.pool_concurrentdict,
-            ParallelIDMappings.pool_leftright,
-        ]
+        for f in fs
             name = string(f)
             @assert startswith(name, "pool_")
             impl = name[length("pool_")+1:end]
